a5e8e5db15c7ad1b6dd9204eb61b3cb457fcbbd6
laurentziudmm <laurentziudmm@gmail.com>
2021-04-19 14:05:06 +0300

    Finished Login and Register Form

--- FrontEndAng/src/app/app-routing.module.ts
+++ FrontEndAng/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEndAng/src/app/app.module.ts
+++ FrontEndAng/src/app/app.module.ts
@@ -1,26 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { MyMaterialModule } from './core/material.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import {FormsModule} from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomePageComponent,
     HeaderComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MyMaterialModule,
     AppRoutingModule,
-    FormsModule
+    FlexLayoutModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
