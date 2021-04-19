33f7b964f89cac0a81d2c4d520dc1f62ea19db12
laurentziudmm <laurentziudmm@gmail.com>
2021-04-19 13:29:08 +0300

    after a while

---
+++ FrontEndAng/src/app/register/address.ts
@@ -0,0 +1,10 @@
+export interface Address {
+  country: string;
+  county: string;
+  city: string;
+  streetAddress: string;
+  postalCode: string;
+  phoneNumber: string;
+  emailAddress: string;
+}
+
--- FrontEndAng/src/app/register/register.component.ts
+++ FrontEndAng/src/app/register/register.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {Register} from './register';
+import {InterfaceUtil} from '../core/interfaceUtil';
+import { RegisterService } from '../service/register.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerInterface: Register;
+  message = '';
 
-  ngOnInit(): void {
+  constructor(private registerService: RegisterService, private router: Router) { }
+
+  register(): void{
+    this.registerService.registerUserFromRemote(this.registerInterface).subscribe(
+      data => {console.log('Response received'),
+        this.router.navigate(['login']);
+      },
+      error => {console.log('error occured'),
+        this.message = 'Registration failed! Check required fields';
+      });
   }
 
+  ngOnInit(): void {
+    this.registerInterface = InterfaceUtil.getEmptyRegister();
+  }
 }
