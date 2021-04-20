02c08579adc3922d373cbd317d15b670419163f5
laurentziudmm <laurentziudmm@gmail.com>
2021-04-20 19:02:35 +0300

    Added Liquibase

---
+++ BackEND/src/main/java/backend/config/PersistenceConfig.java
@@ -0,0 +1,30 @@
+package backend.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfig {
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changelog.xml");
+        liquibase.setDataSource(getDataSource());
+        return liquibase;
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/BookingApp");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("root");
+        return dataSourceBuilder.build();
+    }
+}
