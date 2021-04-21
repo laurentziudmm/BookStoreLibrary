c6d1f1186db012e5e5aa3900b1b9c6854b10dd4b
laurentziudmm <laurentziudmm@gmail.com>
2021-04-21 16:54:26 +0300

    Modification on Spring

--- BackEND/src/main/java/backend/BookStoreBackEND.java
+++ BackEND/src/main/java/backend/BookStoreBackEND.java
@@ -3,8 +3,6 @@ package backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class BookStoreBackEND {
     public static void main(String[] args) {
